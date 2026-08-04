85602d9619943d59b95044001eefe2987564248a
Francesco Donelli <fdonelli99@gmail.com>
2026-08-05 01:31:11 +0200

    Blocco cancellazione progetto 2

--- modules/setup/policy.tf
+++ modules/setup/policy.tf
@@ -1,14 +1,14 @@
-#locals {
-#  target_project_id = var.environment == "prod" ? "cloud-platform-northstar" : "cloud-platform-northstar-test"
-#}
-#
-#data "google_project" "current_project" {
-#  project_id = local.target_project_id
-#}
-#
-#resource "google_resource_manager_lien" "prevent_delete" {
-#  parent       = "projects/${data.google_project.current_project.number}"
-#  restrictions = ["resourcemanager.projects.delete"]
-#  origin       = "terraform-automation"
-#  reason       = "Vincolo di non eliminazione per ambiente ${upper(var.environment)}"
-#}
+locals {
+  target_project_id = var.environment == "prod" ? "cloud-platform-northstar" : "cloud-platform-northstar-test"
+}
+
+data "google_project" "current_project" {
+  project_id = local.target_project_id
+}
+
+resource "google_resource_manager_lien" "prevent_delete" {
+  parent       = "projects/${data.google_project.current_project.number}"
+  restrictions = ["resourcemanager.projects.delete"]
+  origin       = "terraform-automation"
+  reason       = "Vincolo di non eliminazione per ambiente ${upper(var.environment)}"
+}
