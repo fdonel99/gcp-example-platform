c15cd967dc9ab23761a9540340a84e4cded18f53
Francesco Donelli <fdonelli99@gmail.com>
2026-08-05 19:38:02 +0200

    Refactoring telegram 3

--- modules/compute_functions/cloud_function_load_tabelle.tf
+++ modules/compute_functions/cloud_function_load_tabelle.tf
@@ -1,3 +1,18 @@
+# 1. Creazione del pacchetto ZIP dal codice sorgente
+data "archive_file" "zip_tables_loading" {
+  type        = "zip"
+  source_dir  = "${path.module}/src/tables_loading" 
+  output_path = "${path.module}/src/tables_loading.zip"
+}
+
+# 2. Caricamento dello ZIP sul bucket dedicato al codice delle funzioni
+resource "google_storage_bucket_object" "upload_zip_tables_loading" {
+  name   = "tables_loading_${data.archive_file.zip_tables_loading.output_md5}.zip"
+  bucket = var.bucket_codice_funzioni_name
+  source = data.archive_file.zip_tables_loading.output_path
+}
+
+# 3. Definizione della Cloud Function
 resource "google_cloudfunctions2_function" "function_tables_loading" {
   project     = var.project_id
   name        = "tables-loading-fn-${var.environment}"
@@ -14,7 +29,7 @@ resource "google_cloudfunctions2_function" "function_tables_loading" {
     source {
       storage_source {
         bucket = var.bucket_codice_funzioni_name
-        object = google_storage_bucket_object.upload_zip_tables_loading.name
+        object = google_storage_bucket_object.upload_zip_tables_loading.name # Ora lo trova!
       }
     }
   }
@@ -32,22 +47,41 @@ resource "google_cloudfunctions2_function" "function_tables_loading" {
     environment_variables = {
       DATASET_ID  = "NORTHSTAR"
       BUCKET_NAME = var.bucket_export_ns_zip_name
-      SHEET_ID    = "1ptH6m4mS6UozgrtRUfoP_wMMwbx7wTiIn1T6eJ0Vy1c" # Aggiunto!
+      SHEET_ID    = "1ptH6m4mS6UozgrtRUfoP_wMMwbx7wTiIn1T6eJ0Vy1c" 
     }
 
     # Variabili d'ambiente SEGRETE (collegate a Secret Manager)
     secret_environment_variables {
       key        = "TELEGRAM_TOKEN"
       project_id = var.project_id
-      secret     = var.telegram_secret_name # <-- Richiama la variabile passata dal main
+      secret     = var.telegram_secret_name 
       version    = "latest"
     }
 
     secret_environment_variables {
       key        = "TELEGRAM_CHAT_ID"
       project_id = var.project_id
-      secret     = var.telegram_chat_id_secret_name # <-- Richiama la variabile passata dal main
+      secret     = var.telegram_chat_id_secret_name 
       version    = "latest"
     }
+  }
+}
+
+# 4. Montaggio del volume GCS Fuse per leggere lo ZIP grande
+resource "null_resource" "mount_gcs_fuse_volume" {
+  triggers = {
+    function_id = google_cloudfunctions2_function.function_tables_loading.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud run services update ${google_cloudfunctions2_function.function_tables_loading.name} \
+        --project=${var.project_id} \
+        --region=${google_cloudfunctions2_function.function_tables_loading.location} \
+        --add-volume=name=bucket-zip,type=cloud-storage,bucket=${var.bucket_export_ns_zip_name} \
+        --add-volume-mount=volume=bucket-zip,mount-path=/mnt/bucket \
+        --execution-environment=gen2 \
+        --quiet
+    EOT
   }
 }
