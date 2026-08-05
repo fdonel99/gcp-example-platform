b28a99d8d7bab5c622861d669b040ef5b6a75938
Francesco Donelli <fdonelli99@gmail.com>
2026-08-05 19:44:52 +0200

    Refactoring telegram 4

--- modules/compute_functions/cloud_function_drive_to_gcp.tf
+++ modules/compute_functions/cloud_function_drive_to_gcp.tf
@@ -46,6 +46,7 @@ resource "google_cloudfunctions2_function" "function_drive_to_gcp" {
     max_instance_request_concurrency = 80
     
     service_account_email            = var.cloud_worker_sa_email
+    environment_variables = {}
 
     secret_environment_variables {
       key        = "TELEGRAM_TOKEN"
