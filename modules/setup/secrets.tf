d17249ee7d43cf943f7888653327ba0190b17127
Francesco Donelli <fdonelli99@gmail.com>
2026-08-06 15:47:22 +0200

    Configurazioni

--- modules/setup/secrets.tf
+++ modules/setup/secrets.tf
@@ -20,6 +20,7 @@ resource "google_secret_manager_secret_iam_member" "worker_telegram_secret_acces
   secret_id = google_secret_manager_secret.telegram_token.id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.cloud_worker.email}"
+  project   = google_secret_manager_secret.telegram_chat_id.project
 }
 
 resource "google_secret_manager_secret" "telegram_chat_id" {
@@ -44,4 +45,5 @@ resource "google_secret_manager_secret_iam_member" "worker_telegram_chat_id_acce
   secret_id = google_secret_manager_secret.telegram_chat_id.id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.cloud_worker.email}"
+  project   = google_secret_manager_secret.telegram_chat_id.project
 }
