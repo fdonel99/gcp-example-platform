f6b8db59b53f6ecba4c12772412b9323ade21911
Francesco Donelli <fdonelli99@gmail.com>
2026-08-07 10:42:57 +0200

    Refactoring anagrafica prodotto 4

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -43,7 +43,7 @@ module "compute_functions" {
   # Variabili passate dagli ALTRI moduli:
   cloud_worker_sa_email          = module.setup.cloud_worker_sa_email
   bucket_codice_funzioni_name    = module.data_storage.bucket_codice_funzioni_name
-  bucket_import_ns_zip_name      = module.data_storage.bucket_export_ns_zip_name
+  bucket_import_ns_zip_name      = module.data_storage.bucket_import_ns_zip_name
   bucket_spese_trasporto_name    = module.data_storage.bucket_spese_trasporto_name
   bucket_infografica_input_name  = module.data_storage.bucket_infografica_input_name
   bucket_infografica_output_name = module.data_storage.bucket_infografica_output_name
